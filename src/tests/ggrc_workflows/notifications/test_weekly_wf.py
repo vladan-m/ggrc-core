f1628cde39d8459f2f64d2fa8f8726f7014097e5
Ivan Kriznar <ivan@simplified.me>
2015-04-05 17:36:07 +0200

    Assigns tasks to users.

--- src/tests/ggrc_workflows/notifications/test_weekly_wf.py
+++ src/tests/ggrc_workflows/notifications/test_weekly_wf.py
@@ -8,20 +8,21 @@ import copy
 from tests.ggrc import TestCase
 
 import os
+from freezegun import freeze_time
+
 from ggrc import db
 from ggrc_workflows.models import Workflow, TaskGroup, CycleTaskGroupObjectTask, Cycle
+from tests.ggrc.generators.person import PersonGenerator
 from tests.ggrc_workflows.generator import WorkflowsGenerator
 from tests.ggrc.api_helper import Api
 from tests.ggrc.generator import GgrcGenerator
 from nose.plugins.skip import SkipTest
-
 from ggrc_workflows import start_recurring_cycles
 
 if os.environ.get('TRAVIS', False):
   random.seed(1)  # so we can reproduce the tests if needed
 
 
-@SkipTest
 class TestOneTimeWorkflowNotification(TestCase):
 
   def setUp(self):
@@ -50,10 +51,11 @@ class TestOneTimeWorkflowNotification(TestCase):
 
       #test if notifications have been made
 
-
-
-
   def create_test_cases(self):
+    self.person_guest = PersonGenerator(role='reader')
+    self.person_reader = PersonGenerator(role='program_reader')
+    self.person_owner = PersonGenerator(role='program_owner')
+
     self.weekly_wf_1 = {
       "title": "weekly thingy",
       "description": "start this many a time",
@@ -61,23 +63,39 @@ class TestOneTimeWorkflowNotification(TestCase):
       "task_groups": [{
           "title": "tg_2",
           "task_group_tasks": [{
+              "title": "monday to friday",
               "description": self.generator.random_str(100),
               "relative_end_day": 1,
               "relative_end_month": None,
               "relative_start_day": 5,
               "relative_start_month": None,
+              "contact": {
+                "href": self.person_guest.get_href(),
+                "id": self.person_guest.get_id(),
+                "type": "Person"
+              },
             }, {
               "title": "monday task",
               "relative_end_day": 1,
               "relative_end_month": None,
               "relative_start_day": 1,
               "relative_start_month": None,
+              "contact": {
+                "href": self.person_reader.get_href(),
+                "id": self.person_reader.get_id(),
+                "type": "Person"
+              },
             }, {
               "title": "weekend task",
               "relative_end_day": 4,
               "relative_end_month": None,
               "relative_start_day": 1,
               "relative_start_month": None,
+              "contact": {
+                "href": self.person_owner.get_href(),
+                "id": self.person_owner.get_id(),
+                "type": "Person"
+              },
             },
           ],
           "task_group_objects": self.random_objects
