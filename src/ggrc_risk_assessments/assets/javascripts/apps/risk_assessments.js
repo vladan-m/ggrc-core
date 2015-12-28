16a3fdec6765e7d1667b245318365f8b94a91eb7
Ivan Kriznar <ivan@simplified.me>
2015-12-28 12:34:09 +0100

    Lint Risk Assessment app

--- src/ggrc_risk_assessments/assets/javascripts/apps/risk_assessments.js
+++ src/ggrc_risk_assessments/assets/javascripts/apps/risk_assessments.js
@@ -5,44 +5,40 @@
  * Maintained By: dan@reciprocitylabs.com
  */
 
-(function ($, CMS, GGRC) {
-  var RiskAssessmentsExtension = {},
-    _risk_assessments_object_types = ['Program'];
-  GGRC.extensions.push(RiskAssessmentsExtension);
+/* eslint camelcase: 0, new-cap: 0, no-new: 0*/
 
+(function ($, CMS, GGRC) {
+  var RiskAssessmentsExtension = {};
+  var riskAssessmentsObjectTypes = ['Program'];
+
+  GGRC.extensions.push(RiskAssessmentsExtension);
   RiskAssessmentsExtension.name = 'risk_assessments';
 
   // Register RA models for use with `infer_object_type`
   RiskAssessmentsExtension.object_type_decision_tree = function () {
     return {
-      'risk_assessment': CMS.Models.RiskAssessment
+      risk_assessment: CMS.Models.RiskAssessment
     };
   };
-
-  CMS.Models.Program.attributes.risk_assessments = 'CMS.Models.RiskAssessment.stubs';
+  CMS.Models.Program.attributes.risk_assessments =
+    'CMS.Models.RiskAssessment.stubs';
 
   // Configure mapping extensions for ggrc_risk_assessments
   RiskAssessmentsExtension.init_mappings = function init_mappings() {
-    var Proxy = GGRC.MapperHelpers.Proxy,
-      Direct = GGRC.MapperHelpers.Direct,
-      Cross = GGRC.MapperHelpers.Cross,
-      Multi = GGRC.MapperHelpers.Multi,
-      CustomFilter = GGRC.MapperHelpers.CustomFilter,
-      Reify = GGRC.MapperHelpers.Reify,
-      Search = GGRC.MapperHelpers.Search;
-
+    var Proxy = GGRC.MapperHelpers.Proxy;
+    var Direct = GGRC.MapperHelpers.Direct;
     var mappings = {
       Program: {
-            _canonical: {
-              risk_assessments: 'RiskAssessment'
-            },
-            risk_assessments: Direct(
-              'RiskAssessment', 'program', 'risk_assessments'),
-          },
+        _canonical: {
+          risk_assessments: 'RiskAssessment'
+        },
+        risk_assessments: Direct(
+          'RiskAssessment', 'program', 'risk_assessments')
+      },
       RiskAssessment: {
-            documents: Proxy(
-              'Document', 'document', 'ObjectDocument', 'documentable', 'object_documents'),
-          },
+        documents: Proxy('Document', 'document',
+          'ObjectDocument', 'documentable', 'object_documents')
+      }
     };
     new GGRC.Mappings('ggrc_risk_assessments', mappings);
   };
@@ -50,35 +46,34 @@
   // Override GGRC.extra_widget_descriptors and GGRC.extra_default_widgets
   // Initialize widgets for risk assessment page
   RiskAssessmentsExtension.init_widgets = function init_widgets() {
-    var descriptor = {},
-      page_instance = GGRC.page_instance(),
-      tree_widgets = GGRC.tree_view.base_widgets_by_type;
+    var descriptor = {};
+    var pageInstance = GGRC.page_instance();
+    var tree_widgets = GGRC.tree_view.base_widgets_by_type;
 
-    _.each(_risk_assessments_object_types, function (type) {
+    _.each(riskAssessmentsObjectTypes, function (type) {
       if (!type || !tree_widgets[type]) {
         return;
       }
       tree_widgets[type] = tree_widgets[type].concat(['RiskAssessment']);
     });
-    if (page_instance && ~can.inArray(page_instance.constructor.shortName, _risk_assessments_object_types)) {
-      descriptor[page_instance.constructor.shortName] = {
+    if (pageInstance && ~can.inArray(pageInstance.constructor.shortName,
+      riskAssessmentsObjectTypes)) {
+      descriptor[pageInstance.constructor.shortName] = {
         risk_assessments: {
           widget_id: 'risk_assessments',
           widget_name: 'Risk Assessments',
           content_controller: GGRC.Controllers.TreeView,
           content_controller_options: {
             mapping: 'risk_assessments',
-            parent_instance: page_instance,
+            parent_instance: pageInstance,
             model: CMS.Models.RiskAssessment,
             show_view: GGRC.mustache_path + '/base_objects/tree.mustache',
-            draw_children: true,
+            draw_children: true
           }
         }
       };
     }
     new GGRC.WidgetList('ggrc_risk_assessments', descriptor);
   };
-
   RiskAssessmentsExtension.init_mappings();
-
 })(this.can.$, this.CMS, this.GGRC);
