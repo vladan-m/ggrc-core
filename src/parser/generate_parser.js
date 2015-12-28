c15da22c400f61521150c7e1caaecfca37e4db1f
Peter Lamut <peter.lamut@niteoweb.com>
2015-12-28 10:11:33 +0100

    Make the src/parser directory ESLint-compliant

--- src/parser/generate_parser.js
+++ src/parser/generate_parser.js
@@ -7,11 +7,11 @@
 */
 
 /*
-Script used to generate ggrc_filter_query_parser.js 
+Script used to generate ggrc_filter_query_parser.js
 
--- updating dev enviroment by running: 
+-- updating dev enviroment by running:
 
-  vagrant provision 
+  vagrant provision
 
 -- manual dev env setup:
 
@@ -23,51 +23,52 @@ to run this script you need node.js and peg.js
 if npm install returns a 404 just run the next command and try again.
   npm config set registry http://registry.npmjs.org/
 
-
 then edit the parser.pegjs file and run the generator
   ./generate_parser.js
- 
+
 to run the tests first install nodeunit
   npm install -g nodeunit
 
 and run with the test folder
   nodeunit test
-  
+
 */
 
-
-var parser,
-    parser_src,
-    ggrc_parser,
-    parser_grammar = '/vagrant/src/parser/parser.pegjs',
-    parser_template_file = '/vagrant/src/parser/parser_template.js', 
-    ggrc_parser_folder = '/vagrant/src/ggrc/assets/javascripts/generated/',
-    ggrc_parser_js_file = 'ggrc_filter_query_parser.js',
-    peg = require('pegjs'),
-    fs = require('fs'),
-    mkdirp = require('mkdirp'),
-    parser_string = fs.readFileSync(parser_grammar, 'utf8'),
-    filter_template = fs.readFileSync(parser_template_file, 'utf8');
+var parserSrc;
+var ggrcParser;
+var parserGrammar = '/vagrant/src/parser/parser.pegjs';
+var parserTemplateFile = '/vagrant/src/parser/parser_template.js';
+var ggrcParserFolder = '/vagrant/src/ggrc/assets/javascripts/generated/';
+var ggrcParserJsFile = 'ggrc_filter_query_parser.js';
+var peg = require('pegjs');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var parserString = fs.readFileSync(parserGrammar, 'utf8');
+var filterTemplate = fs.readFileSync(parserTemplateFile, 'utf8');
 
 // dirty way of making the parser work in node.js without jquery
 root.jQuery = {
-  unique : function(k) {
-    return Object.keys(k.reduce(function(o,v,i) {o[v] = true; return o;}, {}));
+  unique: function (k) {
+    return Object.keys(
+      k.reduce(function (o, v, i) {
+        o[v] = true;
+        return o;
+      }, {})
+    );
   },
-  type : function(o) {
+  type: function (o) {
     return typeof o;
   }
-}
-
+};
 
 console.log('building parser');
-parser = peg.buildParser(parser_string);
-parser_src = peg.buildParser(parser_string, {output: "source"});
+peg.buildParser(parserString);
+parserSrc = peg.buildParser(parserString, {output: 'source'});
 
 console.log('saving parser to js files');
-ggrc_parser = filter_template.replace('"GENERATED_PLACEHOLDER"', parser_src);
+ggrcParser = filterTemplate.replace('"GENERATED_PLACEHOLDER"', parserSrc);
 
-mkdirp.sync(ggrc_parser_folder);
-fs.writeFileSync(ggrc_parser_folder + ggrc_parser_js_file, ggrc_parser);
+mkdirp.sync(ggrcParserFolder);
+fs.writeFileSync(ggrcParserFolder + ggrcParserJsFile, ggrcParser);
 
 console.log('\ndone :)');
