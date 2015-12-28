3fbbb6efb3718ce02a6c9608f45c807c91923602
Ivan Kriznar <ivan@simplified.me>
2015-12-28 12:30:27 +0100

    Lint Risk Assessment models

--- src/ggrc_risk_assessments/assets/javascripts/models/risk_assessment.js
+++ src/ggrc_risk_assessments/assets/javascripts/models/risk_assessment.js
@@ -5,12 +5,10 @@
  * Maintained By: ivan@reciprocitylabs.com
  */
 
-/*
- */
+/* eslint camelcase: 0, new-cap: 0 */
 
 (function (can) {
-
-  _mustache_path = GGRC.mustache_path + '/risk_assessments';
+  var mustachePath = GGRC.mustache_path + '/risk_assessments';
   can.Model.Cacheable('CMS.Models.RiskAssessment', {
     root_object: 'risk_assessment',
     root_collection: 'risk_assessments',
@@ -29,18 +27,20 @@
       program: 'CMS.Models.Program.stub',
       modified_by: 'CMS.Models.Person.stub',
       object_documents: 'CMS.Models.ObjectDocument.stubs',
-      custom_attribute_values: 'CMS.Models.CustomAttributeValue.stubs',
+      custom_attribute_values: 'CMS.Models.CustomAttributeValue.stubs'
     },
     tree_view_options: {
-      add_item_view: _mustache_path + '/tree_add_item.mustache',
+      add_item_view: mustachePath + '/tree_add_item.mustache',
       child_options: [{
         model: 'Document',
         mapping: 'documents',
-        show_view: _mustache_path + '/documents.mustache',
-      }],
+        show_view: mustachePath + '/documents.mustache'
+      }]
     },
     init: function () {
-      this._super && this._super.apply(this, arguments);
+      if (this._super) {
+        this._super.apply(this, arguments);
+      }
       this.validateNonBlank('title');
       this.validateNonBlank('start_date');
       this.validateNonBlank('end_date');
@@ -54,5 +54,4 @@
       return this._super.apply(this, arguments);
     }
   });
-
 })(window.can);
