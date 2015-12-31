556794c06ed8c0e981e7695eee6d00b0aacfa660
Vladan Mitevski <vladan@mvdevelop.com>
2015-12-31 12:28:00 +0100

    Lint Core client record replay file

--- src/ggrc/assets/javascripts/client_record_replay.js
+++ src/ggrc/assets/javascripts/client_record_replay.js
@@ -6,7 +6,6 @@
 */
 
 (function ($) {
-
 /**
  * GGRC.RequestStore caches GGRC API requests in session or local storage.  It
  * is mostly used for generating ad-hoc fixtures to distinguish latency due to
@@ -21,9 +20,10 @@
  * To clear all data:
  *    GGRC.RequestStore.clear()
  */
-  GGRC.RequestStore = function () {
+  GGRC.RequestStore = (function () {
   // From https://www.artandlogic.com/blog/2013/06/ajax-caching-transports-compatible-with-jquery-deferred/
-  var storage = (typeof (sessionStorage) === undefined) ?
+    var key;
+    var storage = (typeof (sessionStorage) === undefined) ?
       (typeof (localStorage) === undefined) ? {
         getItem: function (key) {
           return this.store[key];
@@ -35,119 +35,123 @@
           delete this.store[key];
         },
         clear: function () {
-          for (var key in this.store) {
-            if (this.store.hasOwnProperty(key)) delete this.store[key];
+          for (key in this.store) {
+            if (this.store.hasOwnProperty(key)) {
+              delete this.store[key];
+            }
           }
         },
         store: {}
       } : localStorage : sessionStorage;
 
-  // Transport layer for saving responses from API requests and short-
-  // circuiting later, duplicate requests
-  var record_replay_transport = function (options, _originalOptions, _jqXHR) {
-    var recording = storage.getItem('RequestStore.record'),
-      replaying = storage.getItem('RequestStore.replay');
+    // Transport layer for saving responses from API requests and short-
+    // circuiting later, duplicate requests
+    var record_replay_transport = function (options, _originalOptions, _jqXHR) {
+      var recording = storage.getItem('RequestStore.record');
+      var replaying = storage.getItem('RequestStore.replay');
+      var url;
+      var match;
 
-    if (_originalOptions._canonical_url) {
-      //console.debug("Found re-entrant request: " + _originalOptions._canonical_url);
-      return;
-    }
+      if (_originalOptions._canonical_url) {
+        // console.debug("Found re-entrant request: " + _originalOptions._canonical_url);
+        return;
+      }
 
-    if (options.type !== 'GET') {
-      return;
-    }
+      if (options.type !== 'GET') {
+        return;
+      }
 
-    var url = options.url,
+      url = options.url;
       match = url.match(/^(.*)([?&])_=\d+(?:([?&])(.*))?$/);
 
-    if (match) {
-      if (match[4]) {
-        url = match[1] + match[2] + match[4];
-      } else {
-        url = match[1];
+      if (match) {
+        if (match[4]) {
+          url = match[1] + match[2] + match[4];
+        } else {
+          url = match[1];
+        }
       }
-    }
 
-    _originalOptions._canonical_url = url;
+      _originalOptions._canonical_url = url;
 
-    return {
-      send: function (headers, completeCallback) {
-        var data = null;
-        if (replaying) {
-          data = storage.getItem('RequestStore:' + url);
-          if (data) {
-            console.debug('Using cache: ', url);
-            setTimeout(function () {
-              completeCallback(200, 'success', {json: JSON.parse(data)});
-            }, 1);
-          } else {
-            console.debug('Missed cache: ', url);
-          }
-        }
-
-        if (!data && (!replaying || recording)) {
-          //console.debug('Using server: ', url);
-          jQuery.ajax(_originalOptions).done(function (data, statusText, jqXHR) {
-            if (recording) {
-              console.debug('Recording: ', url);
-              storage.setItem('RequestStore:' + url, JSON.stringify(data));
+      return {
+        send: function (headers, completeCallback) {
+          var data = null;
+          if (replaying) {
+            data = storage.getItem('RequestStore:' + url);
+            if (data) {
+              console.debug('Using cache: ', url);
+              setTimeout(function () {
+                completeCallback(200, 'success', {json: JSON.parse(data)});
+              }, 1);
+            } else {
+              console.debug('Missed cache: ', url);
             }
-            completeCallback(jqXHR.status, statusText, {json: data});
-          }).fail(function () {
-            console.debug('fail', arguments);
-          });
+          }
+
+          if (!data && (!replaying || recording)) {
+            // console.debug('Using server: ', url);
+            jQuery.ajax(_originalOptions)
+            .done(function (data, statusText, jqXHR) {
+              if (recording) {
+                console.debug('Recording: ', url);
+                storage.setItem('RequestStore:' + url, JSON.stringify(data));
+              }
+              completeCallback(jqXHR.status, statusText, {json: data});
+            }).fail(function () {
+              console.debug('fail', arguments);
+            });
+          }
+        },
+        abort: function () {
+          console.debug('Aborted ajax');
         }
-      },
-      abort: function () {
-        console.debug('Aborted ajax');
+      };
+    };
+
+    // Only apply Ajax Transport when requested
+    var enabled = false;
+    var enable_record_replay_transport = function () {
+      if (!enabled) {
+        $.ajaxTransport('json', record_replay_transport);
+        enabled = true;
       }
     };
-  };
 
-  // Only apply Ajax Transport when requested
-  var enabled = false;
-  var enable_record_replay_transport = function () {
-    if (!enabled) {
-      $.ajaxTransport('json', record_replay_transport);
-      enabled = true;
+    // Initialize Ajax Transport if storage record or replay is enabled
+    var recording = storage.getItem('RequestStore.record');
+    var replaying = storage.getItem('RequestStore.replay');
+    if (recording || replaying) {
+      enable_record_replay_transport();
     }
-  };
 
-  // Initialize Ajax Transport if storage record or replay is enabled
-  var recording = storage.getItem('RequestStore.record'),
-    replaying = storage.getItem('RequestStore.replay');
-  if (recording || replaying) {
-    enable_record_replay_transport();
-  }
+    return {
+      set_record: function (state) {
+        if (state) {
+          enable_record_replay_transport();
+          storage.setItem('RequestStore.record', true);
+        } else {
+          storage.removeItem('RequestStore.record');
+        }
+      },
 
-  return {
-    set_record: function (state) {
-      if (state) {
-        enable_record_replay_transport();
-        storage.setItem('RequestStore.record', true);
-      } else {
-        storage.removeItem('RequestStore.record');
+      set_replay: function (state) {
+        if (state) {
+          enable_record_replay_transport();
+          storage.setItem('RequestStore.replay', true);
+        } else {
+          storage.removeItem('RequestStore.replay');
+        }
+      },
+
+      clear: function () {
+        storage.clear();
+      },
+
+      pause: function () {
+        this.set_record(false);
+        this.set_replay(false);
       }
-    },
-
-    set_replay: function (state) {
-      if (state) {
-        enable_record_replay_transport();
-        storage.setItem('RequestStore.replay', true);
-      } else {
-        storage.removeItem('RequestStore.replay');
-      }
-    },
-
-    clear: function () {
-      storage.clear();
-    },
-
-    pause: function () {
-      this.set_record(false);
-      this.set_replay(false);
-    }
-  };
-}();
-
+    };
+  })();
 })(jQuery);
