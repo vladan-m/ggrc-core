011f27ffe0e378fe708297e69a0ce61e78ea2733
Vladan Mitevski <vladan@mvdevelop.com>
2015-12-31 15:32:44 +0100

    Lint Core design file

--- src/ggrc/assets/javascripts/design.js
+++ src/ggrc/assets/javascripts/design.js
@@ -10,59 +10,59 @@
  * ========================================================= */
 
 jQuery(function ($) {
-
-  //render out templates function
+  // render out templates function
   var renderExternalTmpl = function (item) {
     var file = '/design/templates/' + item.name;
     if ($(item.selector).length > 0) {
       $.when($.get(file)).done(function (tmplData) {
         $(item.selector).append(tmplData);
-        //$.templates({ tmpl: tmplData });
-        //$(item.selector).append($.render.tmpl(item.data));
+        // $.templates({ tmpl: tmplData });
+        // $(item.selector).append($.render.tmpl(item.data));
       });
     }
   };
+  var userHasPriviledge;
 
   renderExternalTmpl({name: 'selectorSectionToControl', selector: '#templates', data: {}});
   renderExternalTmpl({name: 'selectorSectionToControlNA', selector: '#templates', data: {}});
 
   $(document).on('click', '#expand_all', function (event) {
-    //$('.row-fluid-slotcontent').show("fast");
+    // $('.row-fluid-slotcontent').show("fast");
     $('.row-fluid-slotcontent').addClass('in');
     $('.expander').addClass('toggleExpanded');
   });
 
   $(document).on('click', '#shrink_all', function (event) {
-    //$('.row-fluid-slotcontent').hide("fast");
+    // $('.row-fluid-slotcontent').hide("fast");
     $('.row-fluid-slotcontent').removeClass('in');
     $('.expander').removeClass('toggleExpanded');
   });
 
-  /*Checkbutton in modals widget function*/
+  /* Checkbutton in modals widget function */
   $(document).on('click', '.checkbutton', function (event) {
     $(this).children('i').toggleClass('grcicon-blank');
     $(this).children('i').toggleClass('grcicon-x-grey');
   });
 
-  /*Toggle widget function*/
+  /* Toggle widget function */
   $(document).on('click', '.accordion-toggle', function (event) {
     $(this).children('i').toggleClass('grcicon-blue-expand');
   });
 
-  /*Toggle slot function*/
+  /* Toggle slot function */
   // Handled by rotation of expander icon
-  /*$(document).on("click", ".toggle", function(event){
+  /* $(document).on("click", ".toggle", function(event){
     $(this).children(".expander").toggleClass("toggleExpanded");
-  });*/
+  }); */
 
   $(document).on('click', '.expandAll', function (event) {
     // $("h3.trigger").toggleClass("active").next().slideToggle("fast");
     $(this).children('i').toggleClass('grcicon-blue-expand');
   });
 
-  //Handle remove buttons
+  // Handle remove buttons
   $(document).on('click', '.removeCircleButton', function (event) {
-    //alert("here");
+    // alert("here");
     $('#confirmModal').on('hidden', function () {
       $(this).closest('.controlSlot').remove();
     });
@@ -98,48 +98,46 @@ jQuery(function ($) {
     $('#programinformationUnlocked').tab('show');
   });
 
-  //if includes a xpander we need to toggle it.
+  // if includes a xpander we need to toggle it.
   $('body').on('click', '.grcicon-more', function (e) {
-  //bootstrap data toggle opens this one up, but ...
+    var $this = $(this);
+    var slotcontent = $this.closest('.slot')
+      .find('.slottitle').attr('data-target');
+    var $thisexpander = $this.closest('.slot').find('.expander');
+  // bootstrap data toggle opens this one up, but ...
 
     e.preventDefault();
 
-  //var title = $("em").attr("data-target");
-
-    var $this = $(this),
-    slotcontent = $this.closest('.slot').find('.slottitle').attr('data-target'),
-    $thisexpander = $this.closest('.slot').find('.expander');
+  // var title = $("em").attr("data-target");
 
     if (!$thisexpander.hasClass('in')) {
-
-    $thisexpander.addClass('in');
-    $(slotcontent).collapse('show');
-  } else if ($thisexpander) {
-    //$thisexpander.removeClass('in');
-    //$(slotcontent).collapse('hide');
-
-  }
+      $thisexpander.addClass('in');
+      $(slotcontent).collapse('show');
+    } else if ($thisexpander) {
+      // $thisexpander.removeClass('in');
+      // $(slotcontent).collapse('hide');
+    }
 
     $('[id$=-more]').each(function (i) {
-    var el = $(this);
-    if (el.hasClass('in')) {
-      //its open close it!
-      el.collapse('hide');
-    } else {
-      //its already closed, relax.
-      // We have an isue with items on their way to closing and you clicked another, so checking for animating...
-      el.filter(':animated').toggleClass('in');
-    }
-  });
+      var el = $(this);
+      if (el.hasClass('in')) {
+        // its open close it!
+        el.collapse('hide');
+      } else {
+        // its already closed, relax.
+        // We have an isue with items on their way to closing and you clicked another, so checking for animating...
+        el.filter(':animated').toggleClass('in');
+      }
+    });
   });
 
   // show/hide widget
 
   $('body').on('click', '.widget-trigger', function (e) {
+    var $this = $(this);
+    var $widgetBody = $this.closest('.widget').find('.widget-body');
+    var $icon = $this.find('i');
     e.preventDefault();
-    var $this = $(this),
-      $widgetBody = $this.closest('.widget').find('.widget-body'),
-      $icon = $this.find('i');
 
     if ($this.hasClass('active')) {
       $widgetBody.slideUp('fast');
@@ -154,16 +152,14 @@ jQuery(function ($) {
         .removeClass('icon-chevron-right')
         .addClass('icon-chevron-down');
     }
-
   });
 
   // show/hide list item
-
   $('body').on('click', '.list-trigger', function (e) {
+    var $this = $(this);
+    var $listBody = $this.closest('.wlist-title').next();
+    var $icon = $this.find('i');
     e.preventDefault();
-    var $this = $(this),
-      $listBody = $this.closest('.wlist-title').next(),
-      $icon = $this.find('i');
 
     if ($this.hasClass('active')) {
       $listBody.slideUp('fast');
@@ -178,27 +174,25 @@ jQuery(function ($) {
         .removeClass('icon-chevron-right')
         .addClass('icon-chevron-down');
     }
-
   });
 
   // nicer hover
 
   $('body').on('mouseenter', '.people-list li', function (e) {
     $(this).removeClass('.halfopacity');
-    //$(this).find(".additional").slideDown("fast");
+    // $(this).find(".additional").slideDown("fast");
   });
 
   $('body').on('mouseleave', '.people-list li', function (e) {
     $(this).addClass('.halfopacity');
-    //$(this).find(".additional").slideUp("fast");
+    // $(this).find(".additional").slideUp("fast");
   });
 
   $('body').on('click', '.people-list li', function (e) {
-
     if ($(this).find('.additional').hasClass('shown')) {
 
     } else {
-      //brute force ugly
+      // brute force ugly
       $('.additional').slideUp('shown');
       $('.additional').removeClass('shown');
       $(this).find('.additional').slideDown('fast');
@@ -207,42 +201,44 @@ jQuery(function ($) {
   });
 
   // add item in target list
-
   $('body').on('click', '.add-me', function (e) {
-
+    var $this = $(this);
+    var $icon = $this.find('i');
+    var $itemToAdd = $this.closest('li');
+    var $name = $itemToAdd.find('.name').html();
+    var $company = $itemToAdd.find('.company').html();
+    var $target = $this.closest('.modal-body').find('.target');
+    var $unassignedItems = $('#unassignedElements');
+    var $unassignedValue = parseInt($unassignedItems.html(), 10);
+    var $additionalinfo = '';
+    var $item2add = '';
     e.preventDefault();
 
-    var $this = $(this),
-      $icon = $this.find('i'),
-      $itemToAdd = $this.closest('li'),
-      $name = $itemToAdd.find('.name').html(),
-      $company = $itemToAdd.find('.company').html(),
-      $target = $this.closest('.modal-body').find('.target'),
-      $unassignedItems = $('#unassignedElements'),
-      $unassignedValue = parseInt($unassignedItems.html());
-
-    var $additionalinfo = '';
-    if ($this.closest('.modal-body').find('#currentList').hasClass('category-list')) {
-      //Nothing
-      //Pending approval, nothing in additional info
-      var $item2add = '<h6 class="itemstatus">Added</h6>';
-    } else if ($this.closest('.modal-body').find('#currentList').hasClass('people-list')) {
-      //We gave relationships
-      //Pending approval, start-stop
-      var $item2add = '<div class="btn-group inline"> <a class="span7 btn btn-danger btn-mini dropdown-toggle nominheight fltrt" data-toggle="dropdown"> Select Relationship <span class="caret"></span> </a> <ul class="dropdown-menu"> <li> <a href="#" id="makeAccountable"> is Accountable for </a> </li> <li> <a href="#" id="makeResponsible"> is Responsible for </a> </li> </ul> </div>';
-      var $additionalinfo = '<div class="row-fluid additional"> <div class="span4"></div> <div class="span4"> <label>Start Date (Optional)</label> <input class="span12 date" id="datepicker-stopdate-rd" placeholder="MM/DD/YYYY" type="text"> </div> <div class="span4"> <label>Stop Date (Optional)</label> <input class="span12 date" id="datepicker-stopdate-rd" placeholder="MM/DD/YYYY" type="text"> </div>';
-    } else if ($this.closest('.modal-body').find('#currentList').hasClass('reference-list')) {
-      //Will have assignment items, nothing now.
-      //Pending approval, no start-stop
-      var $item2add = '<h6 class="itemstatus">Added</h6>';
-    } else if ($this.closest('.modal-body').find('#currentList').hasClass('system-list')) {
-      //Will have assignment items, nothing now.
-      //Pending approval, no start-stop
-      var $item2add = '<h6 class="itemstatus">Added</h6>';
+    if ($this.closest('.modal-body')
+      .find('#currentList').hasClass('category-list')) {
+      $item2add = '<h6 class="itemstatus">Added</h6>';
+      // Nothing
+      // Pending approval, nothing in additional info
+    } else if ($this.closest('.modal-body')
+      .find('#currentList').hasClass('people-list')) {
+      // We gave relationships
+      // Pending approval, start-stop
+      $item2add = '<div class="btn-group inline"> <a class="span7 btn btn-danger btn-mini dropdown-toggle nominheight fltrt" data-toggle="dropdown"> Select Relationship <span class="caret"></span> </a> <ul class="dropdown-menu"> <li> <a href="#" id="makeAccountable"> is Accountable for </a> </li> <li> <a href="#" id="makeResponsible"> is Responsible for </a> </li> </ul> </div>';
+      $additionalinfo = '<div class="row-fluid additional"> <div class="span4"></div> <div class="span4"> <label>Start Date (Optional)</label> <input class="span12 date" id="datepicker-stopdate-rd" placeholder="MM/DD/YYYY" type="text"> </div> <div class="span4"> <label>Stop Date (Optional)</label> <input class="span12 date" id="datepicker-stopdate-rd" placeholder="MM/DD/YYYY" type="text"> </div>';
+    } else if ($this.closest('.modal-body')
+      .find('#currentList').hasClass('reference-list')) {
+      // Will have assignment items, nothing now.
+      // Pending approval, no start-stop
+      $item2add = '<h6 class="itemstatus">Added</h6>';
+    } else if ($this.closest('.modal-body')
+      .find('#currentList').hasClass('system-list')) {
+      // Will have assignment items, nothing now.
+      // Pending approval, no start-stop
+      $item2add = '<h6 class="itemstatus">Added</h6>';
     }
     $this
       .removeClass('widgetbtn addme')
-      .addClass('widgetbtnoff'); //remove icon square around checkmark (not a button anymore)
+      .addClass('widgetbtnoff'); // remove icon square around checkmark (not a button anymore)
     $icon
       .removeClass('grcicon-chevron-right')
       .addClass('grcicon-check-green');
@@ -260,36 +256,47 @@ jQuery(function ($) {
 
   $('body').on('click', '#makeAccountable', function (e) {
     e.preventDefault();
-    $(this).closest('.btn-group').find('.dropdown-toggle').html('is Accountable');
-    $(this).closest('.btn-group').find('.dropdown-toggle').removeClass('btn-warning');
-    $(this).closest('.btn-group').find('.dropdown-toggle').addClass('btn-success');
+    $(this).closest('.btn-group')
+      .find('.dropdown-toggle').html('is Accountable');
+    $(this).closest('.btn-group')
+      .find('.dropdown-toggle').removeClass('btn-warning');
+    $(this).closest('.btn-group')
+      .find('.dropdown-toggle').addClass('btn-success');
 
-    $(this).closest('.btn-group').find('.dropdown-toggle').addClass('halfopacity');
-    $(this).closest('.btn-group').find('.dropdown-toggle').removeClass('btn-info');
-
+    $(this).closest('.btn-group')
+      .find('.dropdown-toggle').addClass('halfopacity');
+    $(this).closest('.btn-group')
+      .find('.dropdown-toggle').removeClass('btn-info');
   });
 
   $('body').on('click', '#makeResponsible', function (e) {
     e.preventDefault();
-    $(this).closest('.btn-group').find('.dropdown-toggle').html('is Responsible');
-    $(this).closest('.btn-group').find('.dropdown-toggle').removeClass('btn-warning');
-    $(this).closest('.btn-group').find('.dropdown-toggle').addClass('btn-success');
+    $(this).closest('.btn-group')
+      .find('.dropdown-toggle').html('is Responsible');
+    $(this).closest('.btn-group')
+      .find('.dropdown-toggle').removeClass('btn-warning');
+    $(this).closest('.btn-group')
+      .find('.dropdown-toggle').addClass('btn-success');
 
-    $(this).closest('.btn-group').find('.dropdown-toggle').addClass('halfopacity');
-    $(this).closest('.btn-group').find('.dropdown-toggle').addClass('btn-info');
-
+    $(this).closest('.btn-group')
+      .find('.dropdown-toggle').addClass('halfopacity');
+    $(this).closest('.btn-group')
+      .find('.dropdown-toggle').addClass('btn-info');
   });
 
   $(document).on('click', '#removeMe', function (event) {
     event.preventDefault();
-    $(this).closest('li').slideUp('slow', function () { jQuery(this).remove(); });
+    $(this).closest('li').slideUp('slow', function () {
+      jQuery(this).remove();
+    });
   });
 
   $('body').on('click', '#showFilters', function (e) {
+    var $this = $(this);
+    var $filters = $this.closest('.modal-body').find('.filter-group');
+    var $searchableLists = $this.closest('.modal-body')
+      .find('.filter-block .people-list');
     e.preventDefault();
-    var $this = $(this),
-      $filters = $this.closest('.modal-body').find('.filter-group'),
-      $searchableLists = $this.closest('.modal-body').find('.filter-block .people-list');
 
     if ($this.hasClass('active')) {
       $filters.hide();
@@ -303,10 +310,11 @@ jQuery(function ($) {
   });
 
   $('body').on('click', '#showCategories', function (e) {
+    var $this = $(this);
+    var $filters = $this.closest('.modal-body').find('.category-group');
+    var $searchableLists = $this.closest('.modal-body')
+      .find('.filter-block .people-list');
     e.preventDefault();
-    var $this = $(this),
-      $filters = $this.closest('.modal-body').find('.category-group'),
-      $searchableLists = $this.closest('.modal-body').find('.filter-block .people-list');
 
     if ($this.hasClass('active')) {
       $filters.hide();
@@ -320,11 +328,12 @@ jQuery(function ($) {
   });
 
   $('body').on('click', '#showGRCDirectory', function (e) {
+    var $this = $(this);
+    var $categoryfilters = $this.closest('.modal-body').find('.category-group');
+    var $companyfilters = $this.closest('.modal-body').find('.filter-group');
+    var $searchableLists = $this.closest('.modal-body')
+      .find('.filter-block .people-list');
     e.preventDefault();
-    var $this = $(this),
-      $categoryfilters = $this.closest('.modal-body').find('.category-group'),
-      $companyfilters = $this.closest('.modal-body').find('.filter-group'),
-      $searchableLists = $this.closest('.modal-body').find('.filter-block .people-list');
 
     $categoryfilters.show();
     $companyfilters.hide();
@@ -332,48 +341,48 @@ jQuery(function ($) {
     $('#filterButton').html('GRC Directory <span class=\'caret\'></span>');
     $searchableLists.removeClass('shortest');
     $searchableLists.addClass('short');
-    //}
+    // }
   });
 
   $('body').on('click', '#showCompanyDirectory', function (e) {
+    var $this = $(this);
+    var $categoryfilters = $this.closest('.modal-body').find('.category-group');
+    var $companyfilters = $this.closest('.modal-body').find('.filter-group');
+    var $searchableLists = $this.closest('.modal-body')
+        .find('.filter-block .people-list');
     e.preventDefault();
-    var $this = $(this),
-      $categoryfilters = $this.closest('.modal-body').find('.category-group'),
-      $companyfilters = $this.closest('.modal-body').find('.filter-group'),
-      $searchableLists = $this.closest('.modal-body').find('.filter-block .people-list');
 
-    //if( $this.hasClass("active") ) {
-    //$filters.hide();
-    //$this.removeClass("active");
-    //$searchableLists.removeClass("short");
-    //} else {
+    // if( $this.hasClass("active") ) {
+    // $filters.hide();
+    // $this.removeClass("active");
+    // $searchableLists.removeClass("short");
+    // } else {
     $categoryfilters.hide();
     $companyfilters.show();
     $this.addClass('active');
     $('#filterButton').html('Company Directory <span class=\'caret\'></span>');
     $searchableLists.addClass('shortest');
     $searchableLists.removeClass('short');
-    //}
+    // }
   });
 
   // status js
-  var userHasPriviledge = true;
+  userHasPriviledge = true;
   $('body').on('click', '#actionButton', function (e) {
-    e.preventDefault();
-
     var fullDate = new Date();
-    var twoDigitMonth = ((fullDate.getMonth().length + 1) === 1) ? (fullDate.getMonth() + 1) : '0' + (fullDate.getMonth() + 1);
+    var twoDigitMonth = ((fullDate.getMonth().length + 1) === 1) ?
+      (fullDate.getMonth() + 1) : '0' + (fullDate.getMonth() + 1);
     var currentDate = fullDate.getDate() + '/' + twoDigitMonth + '/' + fullDate.getFullYear();
 
-    var $this = $(this),
-      $alert = $this.closest('.modal').find('.alert'),
-      $date = $this.closest('.modal').find('#updatedDate'),
-      $alertMessage = $this.closest('.modal').find('#alertMessage'),
-      $status = $this.closest('.modal').find('#statusValue'),
-      $currentStatus = $this.closest('.modal').find('#statusValue').html();
+    var $this = $(this);
+    var $alert = $this.closest('.modal').find('.alert');
+    var $date = $this.closest('.modal').find('#updatedDate');
+    var $alertMessage = $this.closest('.modal').find('#alertMessage');
+    var $status = $this.closest('.modal').find('#statusValue');
+    var $currentStatus = $this.closest('.modal').find('#statusValue').html();
+    e.preventDefault();
 
     if (userHasPriviledge) {
-
       if ($currentStatus === 'Draft') {
         $status
           .html('Waiting for Approval')
@@ -418,14 +427,14 @@ function toggleGovernance() {
       el.addClass('active');
     }
   });
-
 }
 
 jQuery(function ($) {
-  if ($.cookie('toggle_governance') === '1')
+  if ($.cookie('toggle_governance') === '1') {
     toggleGovernance();
-  else
+  } else {
     $('.govWidget').hide();
+  }
 
   $('body').on('click', '#grcbutton-governance', function (e) {
     toggleGovernance();
