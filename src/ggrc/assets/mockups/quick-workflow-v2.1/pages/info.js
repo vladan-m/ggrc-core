0de5bcbe0cd23242104f0fe514b22e7272f4d4ca
Vladan Mitevski <vladan@mvdevelop.com>
2016-03-11 19:42:56 +0100

    Fix minor visual stuff

--- src/ggrc/assets/mockups/quick-workflow-v2.1/pages/info.js
+++ src/ggrc/assets/mockups/quick-workflow-v2.1/pages/info.js
@@ -9,8 +9,8 @@
     template: '/quick-workflow-v2.1/info.mustache',
     description: Generator.paragraph(8),
     notes: Generator.paragraph(5),
-    manager: 'prasanna@reciprocitylabs.com',
-    prime_contact: 'prasanna@reciprocitylabs.com',
+    manager: 'predrag@reciprocitylabs.com',
+    prime_contact: 'predrag@reciprocitylabs.com',
     sec_contact: 'None',
     tasks: Generator.create({
       title: {
