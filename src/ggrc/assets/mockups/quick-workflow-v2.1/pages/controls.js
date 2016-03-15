65c59960b9e731b478c6e211f8f9282e2c8deedc
Vladan Mitevski <vladan@mvdevelop.com>
2016-03-15 13:51:40 +0100

    Populate controls with correct data

--- src/ggrc/assets/mockups/quick-workflow-v2.1/pages/controls.js
+++ src/ggrc/assets/mockups/quick-workflow-v2.1/pages/controls.js
@@ -19,15 +19,23 @@
       primary_contact: 'sblancha@Reciprocity.com',
       id: '2'
     }, {
-      title: 'Small Sledgehammer',
+      title: 'BR01',
+      info_title: 'BR01',
       type: 'control',
+      description: 'The automated backup systems are configured to send alerts notifications to IT personnel regarding backup job status.  Backup failures are reviewed and managed per documented procedures.',
       id: '3',
       frequency: 'Weekly',
       status: 'Draft',
+      owner: 'sblancha@Reciprocity.com',
+      primary_contact: 'sblancha@Reciprocity.com',
       state: 'Draft'
     }, {
-      title: 'Lonesome Dusty Butter',
+      title: 'TV06',
+      info_title: 'TV06',
       type: 'control',
+      description: 'Information Security audits are performed on new devices prior to deployment into the environment and in accordance with defined configuration standards.',
+      owner: 'sblancha@Reciprocity.com',
+      primary_contact: 'sblancha@Reciprocity.com',
       id: '5',
       frequency: 'Weekly',
       status: 'Draft',
