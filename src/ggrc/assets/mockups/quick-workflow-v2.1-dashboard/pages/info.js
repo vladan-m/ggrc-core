21d1f6a0c4c9fe06006d08dc5be39779dfa4df74
Vladan Mitevski <vladan@mvdevelop.com>
2016-03-24 14:47:57 +0100

    Create workflow modal

--- src/ggrc/assets/mockups/quick-workflow-v2.1-dashboard/pages/info.js
+++ src/ggrc/assets/mockups/quick-workflow-v2.1-dashboard/pages/info.js
@@ -9,9 +9,23 @@
     tasks: Generator.create({
       type: 'task'
     }),
+    workflows: Generator.create({
+      type: 'workflow'
+    }),
     mapped_workflow: [{
       icon: 'workflow',
       title: 'Backlog (One time)'
+    }],
+    repeats: [{
+      value: 'One time'
+    }, {
+      value: 'Weekly'
+    }, {
+      value: 'Monthly'
+    }, {
+      value: 'Quarterly'
+    }, {
+      value: 'Annually'
     }]
   };
 })(GGRC || {}, GGRC.Mockup.Generator);
