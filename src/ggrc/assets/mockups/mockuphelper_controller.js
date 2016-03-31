d84b8b3fed1c5b473a2bd8c81227ebca7104b40c
Vladan Mitevski <vladan@mvdevelop.com>
2016-03-31 16:13:11 +0200

    Add commented section for changing object property value
    I've left this commented so I can remember myself and ask Ivan for
    clarification and instructions on how to make it work.

--- src/ggrc/assets/mockups/mockuphelper_controller.js
+++ src/ggrc/assets/mockups/mockuphelper_controller.js
@@ -354,6 +354,7 @@
     '.show-first-task-trigger click': function (el, ev) {
       $('.zero-state').remove();
       $('.item-main').removeClass('hidden');
+      // GGRC.Bootstrap.Mockups.Workflow2.Sprints.children[0].zero_state = false;
     }
   });
 })(this.can, this.can.$, GGRC.Mockup.Generator);
