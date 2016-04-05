5546da244cffafedd8d22b5462071a0aae88d339
Vladan Mitevski <vladan@mvdevelop.com>
2016-04-05 16:18:13 +0200

    Hide filters by default

--- src/ggrc/assets/mockups/quick-workflow-v2.1-dashboard/pages/markets.js
+++ src/ggrc/assets/mockups/quick-workflow-v2.1-dashboard/pages/markets.js
@@ -5,6 +5,7 @@
   GGRC.Bootstrap.Mockups.QDashboard.Markets = {
     title: 'Markets',
     icon: 'market',
-    template: '/quick-workflow-v2.1/widget.mustache'
+    template: '/quick-workflow-v2.1/widget.mustache',
+    hide_filter: true
   };
 })(GGRC || {}, GGRC.Mockup.Generator);
