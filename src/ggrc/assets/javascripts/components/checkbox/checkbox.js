b8a3ad7201e3d0e9d5371da5433add8fcf094550
Vladan Mitevski <vladan@mvdevelop.com>
2016-04-12 13:15:07 +0200

    Create separate component for checkboxes

---
+++ src/ggrc/assets/javascripts/components/checkbox/checkbox.js
@@ -0,0 +1,22 @@
+/*!
+  Copyright (C) 2016 Google Inc., authors, and contributors <see AUTHORS file>
+  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+  Created By: vladan@reciprocitylabs.com
+  Maintained By: vladan@reciprocitylabs.com
+*/
+
+(function (can, $) {
+  'use strict';
+
+  GGRC.Components('checkbox', {
+    tag: 'checkbox',
+    template: can.view(
+      GGRC.mustache_path +
+      '/components/checkbox/checkbox.mustache'
+    ),
+    scope: {
+      name: '@',
+      value: '@'
+    }
+  });
+})(window.can, window.can.$);
